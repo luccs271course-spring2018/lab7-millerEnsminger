bd57572444ce23ee867e6dff9c80e775a7d6efe4
Taylor MillerEnsminger <tmillerensminger@luc.edu>
2017-10-29 20:54:52 +0000

    the lab stuff add

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -69,16 +69,23 @@ public class LinkedStack<E> implements IStack<E>{
   }
 
   @Override
-  public List<E> asList() {
-    // TODO implement using an ArrayList preallocated with the right size
-    // TODO add any instance variable(s) required to support this
-    Node<E> top1 = top;
-    List<E> list = new ArrayList();
-    while(top1 != null){
-      list.add(top1.data);
-      top1=top1.next;
-    }
-    
-    return list;
-  }
+public List<E> asList() {
+  final ArrayList<E> result = new ArrayList<>(size);
+  populateList(null, result); // TODO replace null with the right reference
+  return result;
+}  
+private void populateList(final Node<E> curr, final List<E> result) {
+  // TODO recursively populate the list in the desired order
+}
+
+@Override
+public List<E> asFifoList() {
+  final ArrayList<E> result = new ArrayList<>(size);
+  populateFifoList(null, result); // TODO replace null with the right reference
+  return result;
+}
+
+private void populateFifoList(final Node<E> curr, final List<E> result) {
+  // TODO recursively populate the list in the desired order
+}
 }
--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -4,24 +4,11 @@ import java.util.Scanner;
 
 public class ReverseLines {
 
-	public static void main(String[] args) {
-		// TODO read successive input lines until EOF, then print out in reverse order
-
-
-	LinkedStack<String> stack = new LinkedStack<String>();
-		
-    final Scanner input = new Scanner(System.in);
-		String line;
-		while ((line = input.nextLine()) != null) {
-			line = input.nextLine();
-			stack.push(line);
-		}
-		String response = ""; 
-		while (!stack.isEmpty()){
-			response = response + stack.pop();
-			System.out.println(response);
-		}
-
-
-	}
+	public static void main(final String[] args) {
+  final Scanner input = new Scanner(System.in);
+  printReverse(input);
+}
+private static void printReverse(final Scanner input) {
+  // TODO recursively read and print successive input lines until EOF, then print them out in reverse order
+} 
 }
