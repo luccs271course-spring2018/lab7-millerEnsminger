9494db3fe5d101768800ab210467af9d86098bbf
Taylor MillerEnsminger <tmillerensminger@luc.edu>
2017-10-31 19:25:31 +0000

    answers

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -1,7 +1,5 @@
 package edu.luc.cs271.linkedstack;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class ReverseLines {
@@ -14,15 +12,13 @@ public class ReverseLines {
   private static void printReverse(final Scanner input) {
     // TODO recursively read and print successive input lines until EOF, then print them out in
     // reverse order
-    System.out.println(input.nextLine());
-    String inputs = input.nextLine();
-    LinkedStack<String> stack = new LinkedStack<String>();
-    List<String> result = new ArrayList<String>();
-    stack.push(inputs);
-    System.out.println(stack.peek());
-    result.add(0, inputs);
-    for (int i = 0; i < result.size(); i++) {
-      System.out.println(result.get(i));
+    // System.out.println(input.nextLine());
+
+    if (input.hasNextLine()) {
+      final String lines = input.nextLine();
+      System.out.println(lines);
+      printReverse(input);
+      System.out.println(lines);
     }
   }
 }
